45c441d5888212410c8a6b404b54aa632418ce81
Mikhail Doshevsky <m.doshevsky@gmail.com>
2014-11-03 16:29:54 +0300

    Implements varying speed

--- ArduPong.ino
+++ ArduPong.ino
@@ -5,10 +5,14 @@
 
 const int LEDS_COUNT = 7;
 const int CENTER_LED = LEDS_COUNT / 2;
+const int MIN_WAIT_VALUE = 80;
+const int MAX_WAIT_VALUE = 250;
+const int WAIT_STEP = (MAX_WAIT_VALUE - MIN_WAIT_VALUE) / (LEDS_COUNT - 1);
 
 int Leds[LEDS_COUNT];
 int CurrentPosition = CENTER_LED;
 bool Direction = true;
+int WaitValue = MAX_WAIT_VALUE;
 bool P1ButtonPreviouseState = false;
 bool P2ButtonPreviouseState = false;
 
@@ -16,6 +20,7 @@ void StartedBlink();
 void HandleButtons();
 bool IsButtonPressed(int pin, bool *previouseState);
 void Reset();
+void SetWaitValue();
 void Finish(int led);
 
 void setup()
@@ -49,7 +54,7 @@ void loop()
   else
   {
     digitalWrite(Leds[CurrentPosition], HIGH);
-    for (int i = 0; i < 200; ++i)
+    for (int i = 0; i < WaitValue; ++i)
     {
       HandleButtons();
       delay(1);
@@ -93,11 +98,13 @@ void HandleButtons()
   if (IsButtonPressed(P1_BUTTON, &P1ButtonPreviouseState))
   {
     Direction = true;
+    WaitValue = MIN_WAIT_VALUE + CurrentPosition * WAIT_STEP;
   }
   
   if (IsButtonPressed(P2_BUTTON, &P2ButtonPreviouseState))
   {
     Direction = false;
+    WaitValue = MIN_WAIT_VALUE + (LEDS_COUNT - CurrentPosition - 1) * WAIT_STEP;
   }
 }
 
@@ -122,6 +129,7 @@ void Reset()
 {  
   CurrentPosition = CENTER_LED;
   Direction = random(0, 2);
+  WaitValue = MAX_WAIT_VALUE;
   P1ButtonPreviouseState = false;
   P2ButtonPreviouseState = false;
   
@@ -133,6 +141,11 @@ void Reset()
   digitalWrite(Leds[CENTER_LED], HIGH);
   delay(800);
   digitalWrite(Leds[CENTER_LED], LOW);
+}
+
+void SetWaitValue()
+{
+  
 }
 
 void Finish(int led)
