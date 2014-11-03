00cd62ab315dcda42c724cdf1972ddc273e32158
Mikhail Doshevskiy <mikhail_doshevskiy@etb-lab.com>
2014-11-03 03:54:36 +0300

    Version 1

---
+++ ArduPong.ino
@@ -0,0 +1,158 @@
+#define LEDS_START_POSITION 2
+#define RESET_BUTTON 12
+#define P1_BUTTON 10
+#define P2_BUTTON 11
+
+const int LEDS_COUNT = 7;
+const int CENTER_LED = LEDS_COUNT / 2;
+
+int Leds[LEDS_COUNT];
+int CurrentPosition = CENTER_LED;
+bool Direction = true;
+bool P1ButtonPreviouseState = false;
+bool P2ButtonPreviouseState = false;
+
+void StartedBlink();
+void HandleButtons();
+bool IsButtonPressed(int pin, bool *previouseState);
+void Reset();
+void Finish(int led);
+
+void setup()
+{
+  for (int i = 0; i < LEDS_COUNT; ++i)
+  {
+    Leds[i] = LEDS_START_POSITION + i;
+    pinMode(Leds[i], OUTPUT);
+  }
+  
+  pinMode(RESET_BUTTON, INPUT_PULLUP);
+  pinMode(P1_BUTTON, INPUT_PULLUP);
+  pinMode(P2_BUTTON, INPUT_PULLUP);
+  
+  StartedBlink();
+}
+
+void loop()
+{
+  digitalWrite(Leds[CurrentPosition], LOW);  
+
+  Direction ? ++CurrentPosition : --CurrentPosition;
+  if (CurrentPosition < 0)
+  {
+    Finish(Leds[0]);
+  }
+  else if (CurrentPosition == LEDS_COUNT)
+  {
+    Finish(Leds[LEDS_COUNT - 1]);
+  }
+  else
+  {
+    digitalWrite(Leds[CurrentPosition], HIGH);
+    for (int i = 0; i < 200; ++i)
+    {
+      HandleButtons();
+      delay(1);
+    }
+  }
+}
+
+void StartedBlink()
+{
+  bool value = true;
+  while (true)
+  {    
+    for (int i = 0; i < LEDS_COUNT; ++i)
+    {
+      digitalWrite(Leds[i], value);
+    }
+    
+    for (int i = 0; i < 200; ++i)
+    {
+      if (!digitalRead(RESET_BUTTON))
+      {
+        Reset();
+        return;
+      }
+      
+      delay(1);
+    }
+    
+    value = !value;
+  }
+}
+
+void HandleButtons()
+{
+  if (!digitalRead(RESET_BUTTON))
+  {
+    Reset();
+    return;
+  }
+  
+  if (IsButtonPressed(P1_BUTTON, &P1ButtonPreviouseState))
+  {
+    Direction = true;
+  }
+  
+  if (IsButtonPressed(P2_BUTTON, &P2ButtonPreviouseState))
+  {
+    Direction = false;
+  }
+}
+
+bool IsButtonPressed(int pin, bool *previouseState)
+{
+  bool result = false;
+  
+  if (!*previouseState && !digitalRead(pin))
+  {
+    delay(10);
+    if (!digitalRead(pin))
+    {
+      result = true;
+    }
+  }
+  
+  *previouseState = !digitalRead(pin);
+  return result;
+}
+
+void Reset()
+{  
+  CurrentPosition = CENTER_LED;
+  Direction = random(0, 2);
+  P1ButtonPreviouseState = false;
+  P2ButtonPreviouseState = false;
+  
+  for (int i = 0; i < LEDS_COUNT; ++i)
+  {
+    digitalWrite(Leds[i], LOW);
+  }
+  
+  digitalWrite(Leds[CENTER_LED], HIGH);
+  delay(800);
+  digitalWrite(Leds[CENTER_LED], LOW);
+}
+
+void Finish(int led)
+{
+  bool value = true;
+  while (true)
+  {
+    digitalWrite(led, value);
+    
+    for (int i = 0; i < 200; ++i)
+    {
+      if (!digitalRead(RESET_BUTTON))
+      {
+        Reset();
+        return;
+      }
+      
+      delay(1);
+    }
+    
+    value = !value;
+  }
+}
